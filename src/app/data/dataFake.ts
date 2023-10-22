89d8fd82be121d7540935f5d818505fa9472ecca
karolineterra <terra.karoline1@gmail.com>
2023-10-22 18:20:42 -0300

    paginas do blog dinamicas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,33 @@
+export const dataFake = [
+  {
+    id: "1",
+    photo:
+      'https://miro.medium.com/v2/resize:fit:1400/1*bMgQ8MhbnQexpqHgIgBJPA.png',
+    title: 'Como começar com o Angular',
+    description:
+      'lorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate libero id dolor sodales, ac auctor metus varius. Donec blandit vehicula fringilla.',
+  },
+  {
+    id: "2",
+    photo:
+      'https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2021/11/o-que-e-html.jpg',
+    title: 'Tornando seu HTML mais semântico',
+    description:
+      'lorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate libero id dolor sodales, ac auctor metus varius. Donec blandit vehicula fringilla.',
+  },
+  {
+    id: "3",
+    photo:
+      'https://hermes.dio.me/articles/cover/1caddefa-9a2d-4d9e-aa3d-d71661d9af6d.jpg',
+    title: 'Como dominar o DOM',
+    description:
+      'lorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate libero id dolor sodales, ac auctor metus varius. Donec blandit vehicula fringilla.',
+  },
+  {
+    id: "4",
+    photo: 'https://img-c.udemycdn.com/course/750x422/1930198_34de_3.jpg',
+    title: 'Como centralizar uma div no css',
+    description:
+      'lorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate libero id dolor sodales, ac auctor metus varius. Donec blandit vehicula fringilla.',
+  },
+];
